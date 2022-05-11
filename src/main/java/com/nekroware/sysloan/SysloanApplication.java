8feb6157f93e8cceccf81612fe82bf244b046439
Leobardo A. Diaz Ruiz <katzekro@gmail.com>
2022-05-11 00:13:31 -0500

    First commit

---
+++ src/main/java/com/nekroware/sysloan/SysloanApplication.java
@@ -0,0 +1,13 @@
+package com.nekroware.sysloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nekroware/sysloan/SysloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nekroware.sysloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
